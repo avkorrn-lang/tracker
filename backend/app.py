afa5a40fd1acd1d197c129288867f4d535cdbb8e
Lex <a.v.korrn@gmail.com>
2026-06-06 18:36:57 +0300

    Разрешил кроссдоменные запросы

--- backend/app.py
+++ backend/app.py
@@ -1,32 +1,32 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS   # <-- новая строка
 
 app = Flask(__name__)
+CORS(app)                      # <-- разрешаем кросс-доменные запросы
 
-# Пока храним задачи в обычном списке (в памяти)
 tasks = []
 
 @app.route('/')
 def hello():
     return '<h1>API трекера задач работает</h1>'
 
-# GET — получить список задач
 @app.route('/api/tasks', methods=['GET'])
 def get_tasks():
     return jsonify(tasks)
 
-# POST — добавить задачу
 @app.route('/api/tasks', methods=['POST'])
 def add_task():
-    data = request.get_json()          # парсим JSON из тела запроса
+    data = request.get_json()
     if not data or 'title' not in data:
         return jsonify({'error': 'Нужно поле title'}), 400
 
     task = {
-        'id': len(tasks) + 1,          # простой автоинкремент
+        'id': len(tasks) + 1,
         'title': data['title'],
         'status': 'active'
     }
     tasks.append(task)
-    return jsonify(task), 201          # 201 — "создано"
+    return jsonify(task), 201
+
 if __name__ == '__main__':
     app.run(debug=True)
